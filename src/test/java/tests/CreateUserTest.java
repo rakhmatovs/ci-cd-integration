f7772e8110032c2c376e3520cc0e43e5ba9b7d68
Sardor <srakhmatov20@gmail.com>
2026-03-13 21:17:40 +0500

    Request with error(2)

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -40,6 +40,6 @@ public class CreateUserTest {
                 .post(CREATE_USER_ENDPOINT)
                 .then()
                 .assertThat()
-                ..statusCode(HttpStatus.SC_BAD_REQUEST);
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 }
